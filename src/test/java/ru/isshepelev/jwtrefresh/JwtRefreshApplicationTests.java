766f7277ad9de5f2ae58b3d3b5be8790ab65e146
Ilya <isshepelev2004@gmail.com>
2023-10-29 23:05:12 +0300

    first commit

---
+++ src/test/java/ru/isshepelev/jwtrefresh/JwtRefreshApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.isshepelev.jwtrefresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtRefreshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
